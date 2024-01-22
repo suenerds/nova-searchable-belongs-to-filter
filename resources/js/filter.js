402d25a8328bb18535e8128a29efd6068b32f0bd
Michael <michael.klopf@suenert.de>
2024-01-22 16:53:51 +0100

    Make changes according to nova 4 upgrade guide

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,3 +1,16 @@
-Nova.booting((Vue, router, store) => {
-  Vue.component('nova-searchable-belongs-to-filter', require('./components/Filter'))
-})
+Nova.booting((Vue, router) => {
+  router.addRoutes([
+    {
+      name: "nova-searchable-belongs-to-filter",
+      path: "/nova-searchable-belongs-to-filter",
+      component: require("./components/Filter"),
+    },
+  ]);
+});
+
+Nova.booting((Vue) => {
+  Nova.inertia(
+    "NovaSearchableBelongsToFilter",
+    require("./component/Filter").default
+  );
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
-let mix = require('laravel-mix')
+let mix = require("laravel-mix");
+
+require("./nova.mix");
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/filter.js', 'js')
-  .sass('resources/sass/filter.scss', 'css')
+  .setPublicPath("dist")
+  .js("resources/js/filter.js", "js")
+  .vue({ version: 3 })
+  .css("resources/css/filter.css", "css")
+  .nova("suenerds/nova-searchable-belongs-to-filter");

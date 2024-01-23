f4ce492b8f9abdadbe9592e8b53e3fb5ccd1f51e
Michael <michael.klopf@suenert.de>
2024-01-23 13:24:53 +0100

    Remove addRoutes function which is the old way of using vue routes

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,13 +1,3 @@
-Nova.booting((Vue, router) => {
-  router.addRoutes([
-    {
-      name: "nova-searchable-belongs-to-filter",
-      path: "/nova-searchable-belongs-to-filter",
-      component: require("./components/Filter"),
-    },
-  ]);
-});
-
 Nova.booting((Vue) => {
   Nova.inertia(
     "NovaSearchableBelongsToFilter",
